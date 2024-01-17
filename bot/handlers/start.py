4ae330fab2a11e13c707b3f8268d6c63cbaa18c9
FlacSy <flacsy.x@gmail.com>
2024-01-17 08:34:08 +0100

    Adding users to the database using the start command

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,12 +2,20 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
+from database.database import SQLiteDatabaseManager
 
 async def start_command(message: types.Message):
     # Обработчик команды /start
     await message.answer("Привет! Это YLoader. Я готов к работе!")
 
-    # Если нужно, можно добавить дополнительные действия, связанные с командой /start
+    # Получение информации о пользователе
+    user_id = message.from_user.id
+    username = message.from_user.username
+
+    # Внесение пользователя в базу данных
+    with SQLiteDatabaseManager() as cursor:
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, user_id INTEGER)''')  
+        cursor.execute('''INSERT INTO users (username, user_id) VALUES (?, ?)''', (username, user_id))
 
 # Регистрация обработчика команды /start
 __all__ = ['start_command']
