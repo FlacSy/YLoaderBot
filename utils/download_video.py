7a68bd112625623d299328617d57dca6fe23889d
FlacSy <flacsy.x@gmail.com>
2024-03-20 23:28:20 +0100

    Fix download video

--- utils/download_video.py
+++ utils/download_video.py
@@ -44,31 +44,37 @@ async def convert_video_to_mp3(video_path: str, output_path: str) -> None:
     video_clip.close()
 
 async def download_youtube(url: str, output_path: str = "downloads", message: Optional[types.Message] = None, format: str = "mp4") -> None:
+    try:
+        await download_youtube_with_format(url, output_path, message, format, download_format="mp4[filesize<50M]")
+    except Exception as e:
+        logging.error(f"Error downloading YouTube video: {str(e)}")
+        logging.error(f"HTTP response: {e.response.text if getattr(e, 'response', None) else 'No response'}")
+
+        try:
+            await download_youtube_with_format(url, output_path, message, format, download_format = "18")
+        except Exception as e:
+            logging.error(f"Error downloading YouTube video: {str(e)}")
+
+
+async def download_youtube_with_format(url: str, output_path: str, message: Optional[types.Message], format: str, download_format: str) -> None:
     options = {
-        'format': 'mp4[filesize<50M]',
+        'format': download_format,
         'outtmpl': f'{output_path}/%(title)s.%(ext)s',
     }
 
     with yt_dlp.YoutubeDL(options) as ydl:
-        try:
-            info_dict = ydl.extract_info(url, download=False)
-            title = info_dict.get('title', 'video')
-            filename = ydl.prepare_filename(info_dict)
+        info_dict = ydl.extract_info(url, download=False)
+        title = info_dict.get('title', 'video')
+        filename = ydl.prepare_filename(info_dict)
 
-            ydl.download([url])
+        ydl.download([url])
 
-            if os.path.exists(filename) and message:
-
-                if format == "mp3":
-                    mp3_filename = f"{output_path}/{title}.mp3"
-                    await convert_video_to_mp3(filename, mp3_filename)
-                    await message.answer_voice(voice=types.InputFile(mp3_filename), caption=title)
-                    os.remove(mp3_filename)
-
-                elif format == "mp4":
-                    await message.answer_video(caption=title, video=types.InputFile(filename))
-                    os.remove(filename)
-
-        except Exception as e:
-            logging.error(f"Error downloading YouTube video: {str(e)}")
-            logging.error(f"HTTP response: {e.response.text if getattr(e, 'response', None) else 'No response'}")
+        if os.path.exists(filename) and message:
+            if format == "mp3":
+                mp3_filename = f"{output_path}/{title}.mp3"
+                await convert_video_to_mp3(filename, mp3_filename)
+                await message.answer_voice(voice=types.InputFile(mp3_filename), caption=title)
+                os.remove(mp3_filename)
+            elif format == "mp4":
+                await message.answer_video(caption=title, video=types.InputFile(filename))
+                os.remove(filename)
